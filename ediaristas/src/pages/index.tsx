8891cdb1bfea4615aba033e36c07dffd1b55869f
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2021-10-07 12:00:02 -0300

    Criado Componente UserInformationContainer

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,17 +1,26 @@
-import type { NextPage } from 'next'
-import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment'
-import PageTitle from 'ui/components/data-display/PageTitle/PageTitle'
+import type { NextPage } from "next";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 
 const Home: NextPage = () => {
   return (
     <div>
       <SafeEnvironment />
-      <PageTitle 
-      title={'Conheça os profissionais'}
-      subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
+      <PageTitle
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua localidade"
+        }
+      />
+      <UserInformation
+        name={"Alvaroico"}
+        picture={"https://github.com/alvaroico.png"}
+        rating={3}
+        description={"São Paulo"}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
